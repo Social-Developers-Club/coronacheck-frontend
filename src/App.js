91c1dcefea00de41f6a7b825df38d6611fbc192b
Daniel <dz@danielzeitler.de>
2020-03-21 11:11:37 +0100

    feat: adding react router dom

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import { HomeSearchField } from './components/homeSearchField';
 import { DetailedMap  } from './components/detailedMap';
 import { ErrorPage  } from './components/errorPage';
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { ButtonAppBar } from './components/buttonAppBar';
 
 function App() {
   return (
       <ThemeProvider theme={theme}>
         <Router>
+          <ButtonAppBar />
           <Route path="/" exact component={HomeSearchField} />
           <Route path="/map" component={DetailedMap} />
-          <Route component={ErrorPage} />
+          {/* <Route component={ErrorPage} /> */}
         </Router>
       </ThemeProvider>
   );
